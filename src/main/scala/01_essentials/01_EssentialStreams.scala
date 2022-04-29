2f0c80ad4f442c790afd98d80d2a0e872540b952
Aliyou Sylla <aliyou29@hotmail.com>
2022-04-29 13:51:04 +0200

    wip

--- src/main/scala/01_essentials/01_EssentialStreams.scala
+++ src/main/scala/01_essentials/01_EssentialStreams.scala
@@ -40,7 +40,30 @@ object EssentialStreams {
     finalData.setParallelism(3) // set parallelism in the sink
   }
 
+  final case class FizzBuzzResult(n: Long, output: String)
+
+  // Ex: FizzBuzz
+  def fizzBuzzExercise(): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val numbers                         = env.fromSequence(1, 100)
+
+    val fizzBuzz = numbers
+      .map { n =>
+        val output =
+          if (n % 3 == 0 && n % 5 == 0) "fizzbuzz"
+          else if (n % 3 == 0) "fizz"
+          else if (n % 5 == 0) "buzz"
+          else s"$n"
+        FizzBuzzResult(n, output)
+      }
+      .filter(_.output == "fizzbuzz")
+      .map(_.n)
+
+    fizzBuzz.writeAsText("output/fizzbuzz").setParallelism(1)
+  }
+
   def main(args: Array[String]): Unit =
     // applicationTemplate()
-    demoTransformations()
+    // demoTransformations()
+    fizzBuzzExercise()
 }
